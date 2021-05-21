6647202cedebb7df8da143732c167cac72e74a1f
lizhihao97 <940166841@qq.com>
2021-05-21 19:31:56 +0800

    test

--- params/params.py
+++ params/params.py
@@ -17,7 +17,7 @@ path_to_Wi = ""+"Wi_weights"
 batch_size = 2
 
 #dataset save path
-path_to_preprocess = '/mnt/ACA21355A21322FE/VoxCeleb/saves2'
+path_to_preprocess = '/home/lizh/datasets/preprocess'
 
 #default for Voxceleb
 frame_shape = 224
